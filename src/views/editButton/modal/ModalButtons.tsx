5531f7aa5421c78e08e8b02aaf067afa5dfbb360
Magdalena BenBassatLuszczynska <magdalena@a-i.com>
2022-12-05 21:02:02 +0200

    fix buttons

--- src/views/editButton/modal/ModalButtons.tsx
+++ src/views/editButton/modal/ModalButtons.tsx
@@ -6,7 +6,7 @@ export const ModalButtons: React.FC<{
   clickHandler: () => void;
 }> = ({ closeDialog, clickHandler }) => {
   return (
-    <div className=" flex items-center justify-center border-t border-gray-200 rounded-b-md px-10 pt-4 pb-80 mt-auto gap-4">
+    <div className=" flex items-center justify-center border-t border-gray-200 rounded-b-md px-10 pt-80 pb-80 mt-auto gap-4">
       <Button
         text={"שמור שינויים"}
         handleClick={() => {
