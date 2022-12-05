17e889395ea6a7540af511a70b0df37bc6e644e8
Magdalena BenBassatLuszczynska <magdalena@a-i.com>
2022-12-05 14:50:55 +0200

    minor css changes

--- src/views/editButton/modal/Button.tsx
+++ src/views/editButton/modal/Button.tsx
@@ -6,10 +6,10 @@ export const Button: React.FC<{
   type?: "button" | "reset" | "submit";
 }> = ({ handleClick, text = "close", type = "button" }) => {
   return (
-    <div className="flex flex-1 content-center justify-center self-center">
+    <div className="flex flex-1 self-center">
       <button
         type={type}
-        className="text-white bg-purple-500 hover:bg-purple-600 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-purple-400 dark:hover:bg-purple-500 dark:focus:ring-purple-600"
+        className="flex flex-1 content-center justify-center self-center min-w-10 text-white bg-purple-500 hover:bg-purple-600 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-purple-400 dark:hover:bg-purple-500 dark:focus:ring-purple-600"
         onClick={() => {
           handleClick();
         }}>
