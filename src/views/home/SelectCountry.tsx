a5305916f4f688d8f8ec08ea80356498656aec7f
Magdalena BenBassatLuszczynska <magdalena@a-i.com>
2022-12-11 18:23:50 +0200

    display of countries' names. optional

--- src/views/home/SelectCountry.tsx
+++ src/views/home/SelectCountry.tsx
@@ -1,50 +1,50 @@
-import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Select from "react-select";
 import { countriesNamesList } from "../../data/list_iso3166_a3";
-import {
-  InitialSimActionsState,
-  SelectedCountriesType,
-} from "../../data/types";
 import { setSelectedCountries } from "../../redux/SimActionsSlice";
-import { Button } from "../editButton/modal/Button";
 
-export const SelectCountry = () => {
+export const SelectCountry: React.FC = () => {
   const dispatch = useDispatch();
 
   const { selectedCountries } = useSelector((s: any) => s.simActions);
 
   return (
-    <div className="flex flex-col justify-center px-8 mx-1 mt-4 mb-6">
-      <Select
-        styles={{
-          control: (baseStyles) => ({
-            ...baseStyles,
-            borderColor: "#a854f7",
-          }),
-          option: (baseStyles, state) => ({
-            ...baseStyles,
-            background:
-              state.isSelected || state.isFocused
-                ? state.isSelected
-                  ? "rgba(168, 84, 247, 0.644)"
-                  : "rgba(168, 84, 247, 0.213)"
-                : "white",
-          }),
-        }}
-        options={countriesNamesList}
-        isMulti={true}
-        onChange={(selected) => dispatch(setSelectedCountries(selected))}
-      />
-      <div
-        className="flex content-center justify-center
-      x">
-        sim will be used in :
+    <div className="flex flex-col justify-center px-8 mx-1 mt-4">
+      <div className="mb-4">
+        <Select
+          styles={{
+            control: (baseStyles) => ({
+              ...baseStyles,
+              borderColor: "#a854f7",
+            }),
+            option: (baseStyles, state) => ({
+              ...baseStyles,
+              background:
+                state.isSelected || state.isFocused
+                  ? state.isSelected
+                    ? "rgba(168, 84, 247, 0.644)"
+                    : "rgba(168, 84, 247, 0.213)"
+                  : "white",
+            }),
+          }}
+          options={countriesNamesList}
+          isMulti={true}
+          onChange={(selected) => dispatch(setSelectedCountries(selected))}
+        />
       </div>
-      {selectedCountries.length
-        ? selectedCountries.map((c: any) => <p key={c.value}>{c.label}</p>)
-        : null}
-      <div className="flex my-10"></div>
+      {selectedCountries.length ? (
+        <div className="flex flex-col mb-4 ">
+          <h2 className="flex text-md justify-center uppercase text-purple-600">
+            :תוכלו להישתמש בשים ב
+          </h2>
+
+          {selectedCountries.map((c: any) => (
+            <span key={c.value} className="flex text-sm justify-center ">
+              {c.label}
+            </span>
+          ))}
+        </div>
+      ) : null}
     </div>
   );
 };
