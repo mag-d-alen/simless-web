38e1d047e2784a5173acfd6bed2967aa202ae0ca
Magdalena BenBassatLuszczynska <magdalena@a-i.com>
2022-12-14 20:37:01 +0200

    fix text area input bug

--- src/views/form/FormTextArea.tsx
+++ src/views/form/FormTextArea.tsx
@@ -13,8 +13,8 @@ export const FormTextArea = (props: FieldHookConfig<any>) => {
       <Field
         component="textarea"
         rows="6"
-        className="inline-block border p-4 text-xs border-purple-100 rounded-lg w-full overflow-wrap p-4 resize border-purple-200 focus:ring-purple-500 focus:border-purple-500"
-        style={{ fontFamily: "Lato", fontSize: "2rem" }}
+        className="inline-block border p-4 border-purple-100 rounded-lg w-full overflow-wrap p-4 resize border-purple-200 focus:ring-purple-500 focus:border-purple-500"
+        style={{ fontFamily: "Lato" }}
         {...field}
         {...props}></Field>
       {meta.touched && meta.error ? (
