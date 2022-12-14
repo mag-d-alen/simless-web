b3b26265e8b9766db7750987ec02e2320827538e
Magdalena BenBassatLuszczynska <magdalena@a-i.com>
2022-12-14 19:45:37 +0200

    fix: fixing edit buttons being not centered

--- src/views/editButton/EditButton.tsx
+++ src/views/editButton/EditButton.tsx
@@ -11,9 +11,8 @@ export const EditButton: React.FC<{
 
   return (
     <button
-      className={`${
-        selectedCountries.length < 1 && "bg-gray-100"
-      } flex w-full bg-white items-center justify-center m-2 rounded-lg border border-purple-200/80 text-sm  text-center  disabled:bg-gray-100 hover: border-purple-400 focus:ring-4 focus:outline-none focus:ring-purple-400  pl-5 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800 dark:text-white`}
+      className={`${selectedCountries.length < 1 && "bg-gray-100"} 
+              flex  bg-white items-center justify-center m-2 rounded-lg border border-purple-200/80 text-sm  text-center  disabled:bg-gray-100 hover: border-purple-400 focus:ring-4 focus:outline-none focus:ring-purple-400  pl-5 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800 dark:text-white`}
       onClick={() =>
         selectedCountries.length < 1 ? showToast(true) : handleClick()
       }>
